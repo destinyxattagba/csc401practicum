ff019304ce4be47a8dba6866ed920fb56acf065f
destinyxattagba <dra86@miami.edu>
2025-09-25 16:03:22 -0400

    neW

--- project1.py
+++
@@ -1,9 +0,0 @@
-import random
-def randomize_in_place(array):
-    n = len(array)-1
-    for index in range(len(array)):
-        print("current array: ", array, "current index: ", index)
-        randint = random.randint(index, n)
-        print("Generated random number: ", randint)
-        array[index], array[randint] = array[randint] , array[index]
-        print("new array: ", array)
